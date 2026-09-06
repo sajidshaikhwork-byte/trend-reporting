5f3856b2a0d2e53519038e9c702f1bd3c59ec273
sajidshaikhwork-byte <sajidshaikhwork@gmail.com>
2026-09-06 15:55:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.15
+});
+
+document.querySelectorAll(".fade").forEach((el) => {
+    observer.observe(el);
+});
